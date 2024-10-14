189cd60a5b815f5265024c3c221878077aa3d82d
Petithory Benjamin <benjamin.petithory@acontrechamp.com>
2024-10-14 10:59:42 +0200

    Ajout de la visite virtuelle Marzipano

---
+++ data.js
@@ -0,0 +1,305 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-cave-1",
+      "name": "Cave 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1911.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0582453761717208,
+          "pitch": 0.1390302448817664,
+          "rotation": 0,
+          "target": "1-cave-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cave-3",
+      "name": "cave 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1911.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05702229218972121,
+          "pitch": 0.19606310779756697,
+          "rotation": 0,
+          "target": "2-cave-2"
+        },
+        {
+          "yaw": 1.5375898381443305,
+          "pitch": 0.1305797485098008,
+          "rotation": 0,
+          "target": "3-cave-4"
+        },
+        {
+          "yaw": -1.6759029544974062,
+          "pitch": 0.09954802466538482,
+          "rotation": 0,
+          "target": "0-cave-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cave-2",
+      "name": "Cave 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1911.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cave-4",
+      "name": "cave 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1911.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4580238401667796,
+          "pitch": -0.011149606036513049,
+          "rotation": 0,
+          "target": "5-jardin-devant"
+        },
+        {
+          "yaw": -1.6862164136826294,
+          "pitch": 0.11178679913704848,
+          "rotation": 0,
+          "target": "1-cave-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-entre-jardin-1",
+      "name": "Entrée jardin 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1911.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9861798968473288,
+          "pitch": 0.08795275625250198,
+          "rotation": 0,
+          "target": "0-cave-1"
+        },
+        {
+          "yaw": 1.2888354045990233,
+          "pitch": -0.012078580961874863,
+          "rotation": 0,
+          "target": "5-jardin-devant"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-jardin-devant",
+      "name": "Jardin Devant",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1911.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0785282544446417,
+          "pitch": 0.17314297826381875,
+          "rotation": 0,
+          "target": "3-cave-4"
+        },
+        {
+          "yaw": 2.6502159591780865,
+          "pitch": 0.208297374868323,
+          "rotation": 0,
+          "target": "6-jardin-fond"
+        },
+        {
+          "yaw": -2.735322125903025,
+          "pitch": 0.18494996400554697,
+          "rotation": 0,
+          "target": "6-jardin-fond"
+        },
+        {
+          "yaw": -0.372983995353934,
+          "pitch": 0.2190117162906624,
+          "rotation": 0,
+          "target": "4-entre-jardin-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-jardin-fond",
+      "name": "Jardin fond",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1911.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
